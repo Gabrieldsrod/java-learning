74526b49ac676f466a8a1a325b6a1447214a49f1
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2025-05-28 21:16:59 -0300

    Lógica para processamento de taxa, correção no totalPayment e funcionamento do progama sem Interfaces

--- interfaces_car_rental/src/application/Program.java
+++ interfaces_car_rental/src/application/Program.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 
 import model.entities.CarRental;
 import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
 
 public class Program {
 	public static void main(String[] args) {
@@ -16,9 +18,8 @@ public class Program {
 		
 		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		
-		System.out.println("Entre com os dados do aluguel:");
+		System.out.println("Entre com os dados do aluguel");
 		System.out.print("Modelo do carro: ");
-		sc.nextLine();
 		String carModel = sc.nextLine();
 		System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
 		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
@@ -26,6 +27,24 @@ public class Program {
 		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 		
 		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+		System.out.print("Entre com o preço por hora: ");
+		double pricePerHour = sc.nextDouble();
+		System.out.print("Entre com o preço por dia: ");
+		double pricePerDay = sc.nextDouble();
+		
+		RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+		
+		rentalService.processInvoice(cr);
+		
+		System.out.println("Fatura:");
+		
+		System.out.println("Pagamento básico " + cr.getInvoice().getBasicPayment());
+		System.out.println("Imposto: " + cr.getInvoice().getTax());
+		System.out.println("Pagamento total: " + cr.getInvoice().getTotalPayment());
+		
+
+				
 		
 		sc.close();
 	}
