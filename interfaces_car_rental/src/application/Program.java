b49ac18d4b49e549e8f36e2d06c0828ea676ca6a
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2025-05-28 21:30:51 -0300

    Solução Aluguel de carros utilizando interface

--- interfaces_car_rental/src/application/Program.java
+++ interfaces_car_rental/src/application/Program.java
@@ -39,12 +39,9 @@ public class Program {
 		
 		System.out.println("Fatura:");
 		
-		System.out.println("Pagamento básico " + cr.getInvoice().getBasicPayment());
-		System.out.println("Imposto: " + cr.getInvoice().getTax());
-		System.out.println("Pagamento total: " + cr.getInvoice().getTotalPayment());
-		
-
-				
+		System.out.println("Pagamento básico " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+		System.out.println("Imposto: " +  String.format("%.2f", cr.getInvoice().getTax()));
+		System.out.println("Pagamento total: " +  String.format("%.2f", cr.getInvoice().getTotalPayment()));
 		
 		sc.close();
 	}
